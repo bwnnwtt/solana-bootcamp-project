f4f1cee39c0ed052c0902b5f223ed3ae520c7b7d
puayjianjun <puayjianjun@gmail.com>
2023-09-28 19:51:28 +0800

    added Wallet and fixed styling
    Co-authored-by: Javier Montes <javiermontescarrera@users.noreply.github.com>
    Co-authored-by: Wei Lin <wei1769@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import Canvas from '@/components/canvas'
+import Canvas from '@/components/Canvas'
+import NavBar from '@/components/NavBar'
+import WalletContextProvider from '@/components/WalletContextProvider'
 import { useState } from 'react'
 
 
@@ -12,8 +14,11 @@ export default function Home() {
 
   return (
     <div>
-      <input type='text' value={text} onChange={handleInputTextChange} placeholder='Enter text...'/>
-      <Canvas text={text}/>
+      <WalletContextProvider>
+        <NavBar />
+        <input type='text' value={text} onChange={handleInputTextChange} placeholder='Enter text...'/>
+        <Canvas text={text}/>
+      </WalletContextProvider>
     </div>
   )
 }
