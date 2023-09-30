1e3382938229ae9c7a2984d654199d6cbbc97ceb
Javier Montes Carrera <javier.montes.carrera@gmail.com>
2023-10-01 00:40:02 +0800

    Creating Hirst spot painting NFT based on SNS and name typed
    Creating Hirst spot painting NFT based on SNS and name typed.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Canvas from '@/components/Canvas'
+import HirstCanvas from '@/components/HirstCanvas'
 import NavBar from '@/components/NavBar'
 import SNSList from '@/components/SNSList'
 import { useState } from 'react'
@@ -7,6 +8,7 @@ import { useState } from 'react'
 export default function Home() {
 
   const [text, setText] = useState('')
+  const [domain, setDomain] = useState('')
 
   function handleInputTextChange(e) {
     setText(e.target.value)
@@ -14,8 +16,16 @@ export default function Home() {
 
   function handleSNSClick(e) {
     let val = e.target.value.slice(0, e.target.value.length - 4)
+    setDomain(e.target.value)
     setText(val)
   }
+
+  const [name, setName] = useState('')
+
+  function handleInputNameChange(e) {
+    setName(e.target.value)
+  }
+
 
   return (
     <div>
@@ -23,8 +33,10 @@ export default function Home() {
       <SNSList handleSNSClick={handleSNSClick} />
       <div>
         <input type='text' value={text} onChange={handleInputTextChange} placeholder='Enter text...'/>
+        <input type='text' value={name} onChange={handleInputNameChange} placeholder='Enter your name...'/>
       </div>
       <Canvas text={text}/>
+      <HirstCanvas text={domain} name={name} />
     </div>
   )
 }
